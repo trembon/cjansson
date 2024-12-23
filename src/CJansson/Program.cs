d792942e782f38fbd92079d2b7649870036a81dc
trembon <trembon@hotmail.com>
2024-12-23 14:53:39 +0100

    try add docker

--- src/CJansson/Program.cs
+++ src/CJansson/Program.cs
@@ -19,6 +19,7 @@ namespace CJansson
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://+:5000")
                 .UseStartup<Startup>();
     }
 }

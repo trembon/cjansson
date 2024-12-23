0f28a0c0ec27f83f21db3d012f3598c7435c37f0
trembon <trembon@hotmail.com>
2024-12-23 15:54:01 +0100

    catch startup error in file watcher

--- src/CJansson/Services/FileEventService.cs
+++ src/CJansson/Services/FileEventService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,35 +18,42 @@ namespace CJansson.Services
     {
         private readonly IBlogService blogService;
         private readonly IWebHostEnvironment hostingEnvironment;
-
+        private readonly ILogger<FileEventService> logger;
         private CancellationTokenSource cancellationToken;
         private object triggerLock;
 
-        public FileEventService(IBlogService blogService, IWebHostEnvironment hostingEnvironment)
+        public FileEventService(IBlogService blogService, IWebHostEnvironment hostingEnvironment, ILogger<FileEventService> logger)
         {
             this.blogService = blogService;
             this.hostingEnvironment = hostingEnvironment;
-
+            this.logger = logger;
             this.cancellationToken = new CancellationTokenSource();
             this.triggerLock = new object();
         }
 
         public void Bind()
         {
-            FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = Path.Combine(hostingEnvironment.ContentRootPath, "posts");
-            watcher.IncludeSubdirectories = true;
+            try
+            {
+                FileSystemWatcher watcher = new FileSystemWatcher();
+                watcher.Path = Path.Combine(hostingEnvironment.ContentRootPath, "posts");
+                watcher.IncludeSubdirectories = true;
 
-            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-            watcher.Filter = "*";
+                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                watcher.Filter = "*";
 
-            // Add event handlers.
-            watcher.Changed += Trigger;
-            watcher.Created += Trigger;
-            watcher.Deleted += Trigger;
-            watcher.Renamed += Trigger;
+                // Add event handlers.
+                watcher.Changed += Trigger;
+                watcher.Created += Trigger;
+                watcher.Deleted += Trigger;
+                watcher.Renamed += Trigger;
 
-            watcher.EnableRaisingEvents = true;
+                watcher.EnableRaisingEvents = true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Unable to start file system watcher on posts folder");
+            }
         }
 
         private void Trigger(object sender, EventArgs e)
